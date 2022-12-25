8f63da4509e8e4b9671d1a3fb638d74a39964aa1
Vishwa <11762008+00000vish@users.noreply.github.com>
2022-12-24 18:33:43 -0800

    Add installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,49 @@
 #!/bin/bash
 
-echo "Installing python3-pip? (Y/n)"
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo."
+  exit
+fi
 
-echo "TODO"
+
+printf  "\nOpenVPN-Client-Monitor installer"
+
+printf  "\n\nPython3-pip (Required)\n"
+read -p 'Install python3-pip? (yes/skip): ' pythonpip
+
+if [ $pythonpip = "yes" ]
+  then 
+    printf "\n"
+    sudo apt install python3-pip
+  else  
+    echo "skipping..."
+fi
+
+
+printf  "\nPyinstaller (Required)\n"
+read -p 'Install Pyinstaller? (yes/skip): ' pyinstaller
+
+if [ $pyinstaller = "yes" ]
+  then 
+    printf "\n"
+    python3 -m pip install pyinstaller
+  else  
+    echo "skipping..."
+fi
+
+printf  "\n\nBuilding Binary\n\n"
+pyinstaller -F main.py
+echo "Done."
+
+printf  "\n\nInstall Binary\n"
+sudo cp ./dist/main /usr/bin/openvpn-client-mon
+echo "Done."
+
+printf  "\n\nSetting up Autorstart\n"
+cp ./etc/systemd/system/openvpnclientmon.service /etc/systemd/system/openvpnclientmon.service
+sudo systemctl enable openvpnclientmon.service
+sudo systemctl start openvpnclientmon.service
+echo "Done."
+
+printf  "\n\nOpenVPN-Client-Monitor installer Installation Complete!\n\n"
+echo  "exiting now... bye"
